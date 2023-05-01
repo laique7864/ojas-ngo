6cb75b942cddf04529c66b4119ec3082df91025c
mubashir.khan <mubashirkhan57510@gmail.com>
2023-05-01 04:09:35 -0700

    fix:image

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import './Navbar.css'
+import img from '../images/instagram.png'
+import img2 from '../images/facebook.png'
+import img3 from '../images/twituer.png'
 // import Contain from '../../Component/Navbar/Contain'
 
 const Navbar = () => {
   return (
     <>
-      <div>
-        <h1 className='pl-5'>edasffdsadf</h1>
+      <div className='bg-blue-500 flex items-center'>
+        <div className='flex items-center '>
+          <h1 className='ml-20'>LOGIN</h1>
+        </div>
+        <div className='flex items-center ml-30'>
+          <a href='!'>Follow Us</a>
+          <img src={img} alt='' />
+          <img src={img2} alt='' />
+          <img src={img3} alt='' />
+        </div>
       </div>
       {/* <Contain/> */}
     </>
