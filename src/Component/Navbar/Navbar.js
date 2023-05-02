733bafe7b7d09dfa5ae053beb7931073321767db
mubashir.khan <mubashirkhan57510@gmail.com>
2023-05-02 00:59:39 -0700

    fix:some topic

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -8,16 +8,37 @@ import img3 from '../images/twituer.png'
 const Navbar = () => {
   return (
     <>
-      <div className='bg-blue-500 flex items-center'>
+      <div className='bg-278CEA flex items-center h-9 justify-around' >
         <div className='flex items-center '>
-          <h1 className='ml-20'>LOGIN</h1>
+          <h1 className='ml-20 text-white'>LOGIN</h1>
         </div>
-        <div className='flex items-center ml-30'>
-          <a href='!'>Follow Us</a>
-          <img src={img} alt='' />
-          <img src={img2} alt='' />
-          <img src={img3} alt='' />
+        <div className='flex items-center '>
+          <a href='!' className='text-xs text-white'>Follow Us</a>
+          <img src={img} alt='' className='ml-10' />
+          <img src={img2} alt='' className='ml-5' />
+          <img src={img3} alt='' className='ml-5' />
         </div>
+        <div className='flex justify-end items-center'>
+          <p className='text-white'>Contact:22548963</p>
+          <p className='ml-5 text-white'>helpdesk@xyz.com</p>
+        </div>
+      </div>
+
+      <div>
+        <ul className='flex justify-around items-center'>
+          <li>HOME</li>
+          <li>Verified Members</li>
+          <li>Events</li>
+          <li>Donors list</li>
+          <li>Management Team </li>
+          <li>Ragistration Form</li>
+          <li>Upcoming Events</li>
+          <li>Donation page</li>
+          <li>Company Profile</li>
+          <li>Contact us</li>
+          <li>Login</li>
+          
+        </ul>
       </div>
       {/* <Contain/> */}
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        '278CEA': '#278CEA',
+      },
+    },
   },
   plugins: [],
 }
