a66c31fe37b583c511ed543852093a83321883fe
junaidkhan5423 <junaidkhan62279@gmail.com>
2023-05-03 13:31:39 +0530

    fix:Tostify

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,19 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import './Login.scss'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login =()=>{
+    const [data, setData] = useState([])
+    const [inputField,setInputField]=useState({
+        user:"",
+        email:"",
+        password:"",
+})
+  
+    const handelOnchange=(event)=>{
+     
+        setInputField((prev)=>{
+            let helper = {...prev}
+         helper[event.target.name] = event.target.value
+         return helper
+        })
+   
+    }
+     
+
+    const userhandleSubmite=()=>{
+        console.log(inputField)
+    }
+ 
     return(
         <>
-        <h1 className="heding">Registration Form</h1>
+        <h1 className="heding">User Login</h1>
+        <ToastContainer />
         <div className="main-div">
             <label className="lab-div">Name</label>
-            <input type='text' />
+            <input type='text' name="user" value={inputField.name} onChange={handelOnchange} />
             <label className="lab-div-emsil">Email</label>
-            <input type='text' />
+            <input type='text'  name="email"  value={inputField.email} onChange={handelOnchange} />
             <label className="lab-div">Password</label>
-            <input type='text' />
-            <button className="btn">Login</button>  
+            <input type='text'  name="password" value={inputField.password} onChange={handelOnchange} />
+            
+            <button className="junaid" onClick={userhandleSubmite} ><a>Login</a></button> 
+            
+           
         </div>
+       
         </>
     )
 }
